eb09a520ecee9799dd758fc0078420d1806a738a
prostonau <prostonau@mail.ru>
2023-02-21 00:01:37 +0300

    init | guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0
+        this.max = 0        
+        this.number = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max        
     }
 
     guess() {
+        this.number = Math.round((this.max + this.min) / 2);
+        //console.log('min = ' + this.min + ' max  = ' +this.max + '  guess = ' + this.number )
+        return this.number //409 //this.guess
+
 
     }
 
     lower() {
-
+      //console.log("lower")
+      this.max = this.number
+        
     }
 
     greater() {
-
+        //console.log("greater")
+        this.min = this.number
     }
 }
 
